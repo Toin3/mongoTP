9b7ff83819da23350a1b8c55d4b5625901aab1e8
Florian Fischer <flow@debian7>
2013-01-23 11:51:22 +0100

    Ajout du listing des collections

--- lib/MongoXplorer.pm
+++ lib/MongoXplorer.pm
@@ -3,12 +3,31 @@
 package MongoXplorer;
 use Mojolicious::Lite;
 
+my $g_instance_name;
+my $g_instance_port;
+
+
 sub get_databases {
 	my $self = shift;
 	my ( $instance_name, $instance_port ) = @_;
 	my $mongo_connection = MongoDB::MongoClient->new(host => $instance_name.':'.$instance_port);
+	$g_instance_name = $instance_name;
+	$g_instance_port = $instance_port;
+
 	my @dbs = $mongo_connection->database_names;
 	return @dbs;
+}
+
+sub get_collections
+{
+	my $self = shift;
+	my ($db_name) = @_;
+	my $mongo_connection = MongoDB::MongoClient->new(host => $g_instance_name.':'.$g_instance_port);
+
+	my $database = $mongo_connection->get_database($db_name);
+        my @collections = $database->collection_names;	
+
+	return @collections;
 }
 
 1;
--- mongoxplorer.pl
+++ mongoxplorer.pl
@@ -15,4 +15,11 @@ post '/dblist' => sub {
 	return $self->render('dblist', dbs => [@dbs]);
 };
 
+get '/database/:dbname' => sub {
+    my $self = shift;
+    my $dbname = $self->param('dbname');
+    my @collections = MongoXplorer->get_collections($dbname);
+    $self->render('collectionlist', dbname => $dbname, collections => [@collections]);    
+};
+
 app->start;
