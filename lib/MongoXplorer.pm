364261d27fc74274f4d485aea9a92539f03b6434
Antoine ROGER <antoine.roger@ingesup.com>
2013-01-25 11:40:08 +0100

    Gestion des erreurs de connexion à la base

--- lib/MongoXplorer.pm
+++ lib/MongoXplorer.pm
@@ -14,11 +14,17 @@ my $g_instance_port;
 sub get_databases {
 	my $self = shift;
 	my ( $instance_name, $instance_port ) = @_;
-	my $mongo_connection = MongoDB::MongoClient->new(host => $instance_name.':'.$instance_port);
-	$g_instance_name = $instance_name;
-	$g_instance_port = $instance_port;
-	my @dbs = $mongo_connection->database_names;
-	return @dbs;
+	my $mongo_connection;
+	my $rv = eval { $mongo_connection = MongoDB::MongoClient->new(host => $instance_name.':'.$instance_port) }; 
+	if ($@) {
+		return "instance-not-found";
+	}else
+	{
+		$g_instance_name = $instance_name;
+		$g_instance_port = $instance_port;
+		my @dbs = $mongo_connection->database_names;
+		return @dbs;
+	}   
 }
 
 sub get_collections
@@ -30,6 +36,18 @@ sub get_collections
         my @collections = $database->collection_names;
 	@collections = grep { index($_, '.$_id_') == -1 } @collections;
 	return @collections;
+}
+
+sub get_collection_data
+{
+	my $self = shift;
+	my ( $db_name, $collection_name ) = @_;
+	my $mongo_connection = MongoDB::MongoClient->new(host => $g_instance_name.':'.$g_instance_port);
+	my $db = $mongo_connection->get_database( $db_name );
+	my $collection_content = $db->get_collection( $collection_name );
+	my $data = $collection_content->find_one({ "yahoo.woe" => {'$exists' => 1} });
+	
+	return $data;
 }
 
 1;
--- mongoxplorer.pl
+++ mongoxplorer.pl
@@ -11,7 +11,15 @@ get '/' => sub {
 post '/dblist' => sub {
     my $self = shift;
 	my @dbs = MongoXplorer->get_databases($self->param('instance_name'), $self->param('instance_port'));
-	return $self->render('dblist', dbs => [@dbs], title => 'Liste des bases de données');
+	if (join('', @dbs) eq "instance-not-found")
+	{
+		$self->flash(warning_message => "L'instance n'a pas été trouvée. Vérifiez l'adresse IP et le port de destination.");
+		$self->redirect_to( '/' );
+	}
+	else
+	{
+		$self->render('dblist', dbs => [@dbs], title => 'Liste des bases de données');
+	}
 };
 
 get '/databasename/:collectionname' => sub {
@@ -19,6 +27,12 @@ get '/databasename/:collectionname' => sub {
     my $dbname = $self->param('collectionname');
     my @collections = MongoXplorer->get_collections($dbname);
     $self->render('collectionlist', databasename => $dbname, collections => [@collections], title => 'Collections de la base '.$dbname);
+};
+
+get '/collectiondetails/:databasename/:collectionname' => sub {
+    my $self = shift;
+	my $data = MongoXplorer->get_collection_data($self->param('databasename'), $self->param('collectionname'));
+    $self->render('collectiondetails', title => 'Aperçu de la collection '.$self->param('collectionname'), collectionname => $self->param('collectionname'), collection_content => $data);
 };
 
 
